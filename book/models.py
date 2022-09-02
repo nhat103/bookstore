3945e228552933450882b1c73b0ba2738b21c573
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-09-02 20:14:12 +0700

    add databse

--- book/models.py
+++ book/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=25, null=True)
+    author = models.CharField(max_length=25, null=True)
+    price = models.IntegerField()
+    edition = models.IntegerField()
+
+    class Meta:
+        db_table = "book"
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25, null=True)
+    phone = models.CharField(max_length=25, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        db_table = "customer"
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Cart(models.Model):
+    customer = models.OneToOneField(
+        Customer, null=True, on_delete=models.CASCADE)
+    book = models.ManyToManyField(Book)
+
+    class Meta:
+        db_table = "cart"
+
+    def __str__(self):
+        return str(self.customer)
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book.apps.BookConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'bookstore',
+        'PASSWORD': 'linux12345',
+        'HOST': '127.0.0.1',
     }
 }
 
